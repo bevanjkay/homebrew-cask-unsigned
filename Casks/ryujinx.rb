eccaeb2462971019e4fd1947ec014e8c585d3bb7
Patrick Linnane <patrick@linnane.io>
2022-12-12 08:36:00 -0800

    ryujinx: update sha256, add verified param

--- Casks/ryujinx.rb
+++ Casks/ryujinx.rb
@@ -1,8 +1,9 @@
 cask "ryujinx" do
   version "1.1.0"
-  sha256 "2ab00be8ae763cb8e0e9f0c3746609c16e74a36e4d95bbe72a0b0de2862942fd"
+  sha256 "259d56bbded1940fb227e01d0e8c0b4ad68dc8f8953f19a6414485427d9c7ae9"
 
-  url "https://github.com/Ryujinx/release-channel-macos/releases/download/#{version}-macos1/Ryujinx-#{version}-macos1-macos_universal.app.tar.gz"
+  url "https://github.com/Ryujinx/release-channel-macos/releases/download/#{version}-macos1/Ryujinx-#{version}-macos1-macos_universal.app.tar.gz",
+      verified: "github.com/Ryujinx/release-channel-macos/"
   name "Ryujinx"
   desc "Nintendo Switch emulator"
   homepage "https://ryujinx.org/"
